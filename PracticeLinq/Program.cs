9167a242322c522f4bd458c1a2c96db54b0bbd57
alimarcanotovar <aliymarcanotovar91@gmail.com>
2022-08-26 13:34:26 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>
+           {
+               "Overwatch", "Majoras Mask", "Rust" , "Scum"
+           };
+            IEnumerable<string> videogamenames = videoGames.Where(name => name.Contains("0"));
+
+            //foreach(var i in videoGames)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            var orderOfVideogames = videoGames.OrderBy(name => name.Length).ToList();
+
+            foreach (var i in orderOfVideogames)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+        }
+    }
+}
